0ab65ebf868c215cea5b6498361bae497b81df33
Sergei <sergeigulyako@mail.ru>
2024-04-02 10:42:54 +0300

    Added logic for finding words with the maximum number of digits

---
+++ Homeworks/Homework5-TextAnalyzer/DigitSearcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalyzer
+{
+    internal class DigitSearcher
+    {
+        public string FindWordWithMostDigits(string input)
+        {
+            string[] words = input.Split(" ");
+
+            string wordWithMostDigits = " ";
+            int maxDigitCount = 0;
+
+            foreach (string word in words)
+            {
+                int digitCount = CountDigits(word);
+                if (digitCount > maxDigitCount)
+                {
+                    maxDigitCount = digitCount;
+                    wordWithMostDigits = word;
+                }
+            }
+            return wordWithMostDigits;
+        }
+
+        private int CountDigits(string word)
+        {
+            int count = 0;
+            foreach (char letter in word)
+            {
+                if (char.IsDigit(letter))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
---
+++ Homeworks/Homework5-TextAnalyzer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TextAnalyzer
+{
+    class StringReader
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Введите строку..");
+            string input = Console.ReadLine();
+
+            DigitSearcher digitSearcher = new DigitSearcher();
+
+
+            while (true)
+            {
+                Console.WriteLine("Меню выбора действий: ");
+                Console.WriteLine("1.Найти слова содержащие максимальное количество цифр.");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        string wordWithMostDigits = digitSearcher.FindWordWithMostDigits(input);
+                        Console.WriteLine($"Слово с наибольшим количеством цифр: {wordWithMostDigits}");
+                        break;
+                }
+            }
+        }
+    }
+}
