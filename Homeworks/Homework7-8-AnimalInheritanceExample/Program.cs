14207f7d73c5d28d4455e0b9b4dd5239f74e907c
Sergei <sergeigulyako@mail.ru>
2024-04-06 16:44:34 +0300

    Homework7-8-AnimalInhabitanceExample

---
+++ Homeworks/Homework7-8-AnimalInheritanceExample/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Homework7_8_AnimalInheritanceExample
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the dog's name: ");
+            string dogName = Console.ReadLine();
+
+            Dog dog = new Dog();
+            dog.SetName(dogName);
+
+            Console.WriteLine("The name of the dog is " + dog.GetName());
+            dog.Eat();
+        }
+    }
+}
